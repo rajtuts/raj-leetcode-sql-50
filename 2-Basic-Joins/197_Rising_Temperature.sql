87cea402ca2a7599b478ff4d8afaea16567113db
rajtuts <rajtuts79@gmail.com>
2024-08-02 07:44:02 -0500

    Update 197_Rising_Temperature.sql

--- 2-Basic-Joins/197_Rising_Temperature.sql
+++ 2-Basic-Joins/197_Rising_Temperature.sql
@@ -16,6 +16,30 @@
 
 -- Return the result table in any order.
 
+Example 1:
+
+Input: 
+Weather table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+Output: 
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+Explanation: 
+In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
+
+  
 ------------------------------------------------------------------------------
 
 -- SQL Schema
@@ -32,3 +56,7 @@ insert into Weather (id, recordDate, temperature) values ('4', '2015-01-04', '30
 SELECT w2.id
 FROM Weather w1, Weather w2
 WHERE DATEDIFF(day, w1.recordDate, w2.recordDate) = 1 AND w2.temperature > w1.temperature
+
+-- Oracle Code
+select w.id from Weather w, Weather w2 
+where   w.recordDate=w2.recordDate+1 and w.temperature>w2.temperature
